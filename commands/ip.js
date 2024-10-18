526a191c21c3f9d1ac2d2388094857cabb6e3715
Gestures5 <kenshimadax@gmail.com>
2024-10-19 00:13:28 +0800

    Update ip.js

--- commands/ip.js
+++ commands/ip.js
@@ -2,21 +2,21 @@ const axios = require('axios');
 
 module.exports = {
   name: 'gpt',
-  description: 'ask to gpt4',
+  description: 'interact with gpt-4O',
   author: 'developer',
   async execute(senderId, args, pageAccessToken, sendMessage) {
     const prompt = args.join(' ');
     const uid = senderId; // Assuming uid is the same as senderId; adjust as needed.
-    
+
     try {
-      const apiUrl = `https://markdevs-last-api-2epw.onrender.com/gpt4?prompt=${encodeURIComponent(prompt)}&uid=${uid}`;
+      const apiUrl = `https://ccprojectsjonellapis-production.up.railway.app/api/gpt4o-v2?prompt=${encodeURIComponent(prompt)}&uid=${uid}`;
       const response = await axios.get(apiUrl);
       const text = response.data.result; // Adjust based on the actual response structure
 
       // Send the response back to the user
       sendMessage(senderId, { text }, pageAccessToken);
     } catch (error) {
-      console.error('Error calling GPT4 API:', error);
+      console.error('Error calling GPT-4O API:', error);
       sendMessage(senderId, { text: 'An error occurred while processing your request.' }, pageAccessToken);
     }
   }

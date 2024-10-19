8bb7d49f5506ef687e68c611b2ced8986557acbc
Gestures5 <kenshimadax@gmail.com>
2024-10-19 12:16:25 +0800

    Update claude.js

--- commands/claude.js
+++ commands/claude.js
@@ -1,19 +1,26 @@
 const axios = require('axios');
+
 module.exports = {
   name: 'claude',
-  description: 'ask to caude sonnet',
+  description: 'ask to claude sonnet 3.5',
   author: 'developer',
   async execute(senderId, args, pageAccessToken, sendMessage) {
     const prompt = args.join(' ');
+    
+    if (!prompt) {
+      sendMessage(senderId, { text: 'Please provide a valid question.' }, pageAccessToken);
+      return;
+    }
+
     try {
       const apiUrl = `https://rest-api.joshuaapostol.site/blackbox/model/claude-sonnet-3.5?prompt=${encodeURIComponent(prompt)}`;
       const response = await axios.get(apiUrl);
-      const text = response.data.result;
+      const answer = response.data.response;
 
-      sendMessage(senderId, { text }, pageAccessToken);
+      sendMessage(senderId, { text: `🤖 CLAUDE SONNET 3.5 AI\n━━━━━━━━━━━━━━━━━━\n${answer}\n━━━━━━━━━━━━━━━━━━` }, pageAccessToken);
     } catch (error) {
       console.error('Error calling Claude Sonnet API:', error);
-      sendMessage(senderId, { text: 'There was an error with your request.' }, pageAccessToken);
+      sendMessage(senderId, { text: '❌ | An error occurred while processing your request.' }, pageAccessToken);
     }
   }
 };

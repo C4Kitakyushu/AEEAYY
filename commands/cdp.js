06d6ebd2330325ab52b5c6ad50984fb2b45d8505
Gestures5 <kenshimadax@gmail.com>
2024-10-21 04:12:40 +0800

    Update commands

---
+++ commands/cdp.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+const fs = require('fs');
+
+const token = fs.readFileSync('token.txt', 'utf8');
+
+module.exports = {
+  name: 'cdp',
+  description: 'fetch couple dp images',
+  author: 'developer',
+  usage: 'cdp',
+
+  async execute(senderId) {
+    const pageAccessToken = token;
+
+    try {
+      // Make the API call to get the couple DP data
+      const { data } = await axios.get("https://c-v3.onrender.com/v1/cdp/get");
+
+      // Fetch male image
+      const maleImg = await axios.get(data.male, { responseType: 'arraybuffer' });
+      fs.writeFileSync(__dirname + '/tmp/img1.png', Buffer.from(maleImg.data, 'utf-8'));
+
+      // Fetch female image
+      const femaleImg = await axios.get(data.female, { responseType: 'arraybuffer' });
+      fs.writeFileSync(__dirname + '/tmp/img2.png', Buffer.from(femaleImg.data, 'utf-8'));
+
+      // Send message with both images
+      const msg = 'Here is your couple dp 💜(◕ᴗ◕✿)';
+      const allImages = [
+        fs.createReadStream(__dirname + '/tmp/img1.png'),
+        fs.createReadStream(__dirname + '/tmp/img2.png')
+      ];
+
+      await sendMessage(senderId, { text: msg, attachment: allImages }, pageAccessToken);
+    } catch (error) {
+      console.error('Error fetching couple DP:', error);
+      await sendMessage(senderId, { text: 'Error: Unable to fetch couple DP.' }, pageAccessToken);
+    }
+  },
+};

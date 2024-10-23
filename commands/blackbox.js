4656816b093902c52debfd6e5d2c77338e188106
Gestures5 <kenshimadax@gmail.com>
2024-10-23 23:55:29 +0800

    Update blackbox.js

--- commands/blackbox.js
+++ commands/blackbox.js
@@ -25,7 +25,7 @@ module.exports = {
 ━━━━━━━━━━━━━━━━━━
 ${responseString}
 ━━━━━━━━━━━━━━━━━━
-◉ -,-
+
       `;
 
       sendMessage(senderId, { text: formattedResponse.trim() }, pageAccessToken);

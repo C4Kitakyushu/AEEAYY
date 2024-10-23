5988c60122075b865b70a2701e17e7ff50a7cb11
Gestures5 <kenshimadax@gmail.com>
2024-10-23 19:46:16 +0800

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -12,6 +12,6 @@ module.exports = {
     const days = Math.floor(uptime / (60 * 60 * 24));
     const uptimeString = `${days} 𝗗𝗮𝘆𝘀 ${hours} 𝗛𝗼𝘂𝗿𝘀 ${minutes} 𝗠𝗶𝗻𝘂𝘁𝗲𝘀 ${seconds} 𝗦𝗲𝗰𝗼𝗻𝗱𝘀;
 
-    sendMessage(senderId, { text: `𝗠𝗘𝗧𝗔𝗟𝗟𝗜𝗖 𝗖𝗛𝗥𝗢𝗠𝗘 𝗨𝗣𝗧𝗜𝗠𝗘🟢\\➜${uptimeString}` }, pageAccessToken);
+    sendMessage(senderId, { text:` 𝗠𝗘𝗧𝗔𝗟𝗟𝗜𝗖 𝗖𝗛𝗥𝗢𝗠𝗘 𝗨𝗣𝗧𝗜𝗠𝗘🟢n\n\➜${uptimeString}` }, pageAccessToken);
   }
 };

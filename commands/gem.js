eb2143ef4ce89418bffad87873db6cea310937a9
Gestures5 <kenshimadax@gmail.com>
2024-10-25 00:05:29 +0800

    Update commands

---
+++ commands/gem.js
@@ -0,0 +1,82 @@
+const axios = require("axios");
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: "gemini",
+  description: "Get description of an image using the Gemini API.",
+  author: "developer",
+
+  async execute(metallic, bot, version, event) {
+    const versionPrompt = bot.join(" ");
+
+    if (!versionPrompt) {
+      return sendMessage(metallic, { text: `Please provide an image and reply to it using messenger or ask a question!\n\nExample: gemini what is AI?` }, version);
+    }
+
+    sendMessage(metallic, { text: "Please wait... 🔎" }, version);
+
+    try {
+      let imageUrl = "";
+
+      if (event.message.reply_to && event.message.reply_to.mid) {
+        imageUrl = await getRepliedImage(event.message.reply_to.mid, version);
+      } else if (event.message?.attachments && event.message.attachments[0]?.type === 'image') {
+        imageUrl = event.message.attachments[0].payload.url;
+      }
+
+      const apiUrl = `https://joshweb.click/gemini`;
+
+      const geminiResponse = await handleImageRecognition(apiUrl, versionPrompt, imageUrl);
+      const result = geminiResponse.gemini;
+
+      sendConcatenatedMessage(metallic, result, version);
+
+    } catch (error) {
+      console.error("Error in Gemini command:", error);
+      sendMessage(metallic, { text: `Error: ${error.message || "Something went wrong."}` }, version);
+    }
+  }
+};
+
+async function handleImageRecognition(apiUrl, prompt, imageUrl) {
+  const { data } = await axios.get(apiUrl, {
+    params: {
+      prompt,
+      url: imageUrl || ""
+    }
+  });
+
+  return data;
+}
+
+async function getRepliedImage(mid, version) {
+  const { data } = await axios.get(`https://graph.facebook.com/v21.0/${mid}/attachments`, {
+    params: { access_token: version }
+  });
+
+  if (data && data.data.length > 0 && data.data[0].image_data) {
+    return data.data[0].image_data.url;
+  } else {
+    return "";
+  }
+}
+
+async function sendConcatenatedMessage(metallic, text, version) {
+  const maxMessageLength = 2000;
+
+  if (text.length > maxMessageLength) {
+    const messages = splitMessageIntoChunks(text, maxMessageLength);
+
+    for (let i = 0; i < messages.length; i++) {
+      await new Promise(resolve => setTimeout(resolve, 1000)); // 1-second delay
+      await sendMessage(metallic, { text: messages[i] }, version);
+    }
+  } else {
+    await sendMessage(metallic, { text }, version);
+  }
+}
+
+function splitMessageIntoChunks(message, chunkSize) {
+  const regex = new RegExp(`.{1,${chunkSize}}`, 'g');
+  return message.match(regex);
+}

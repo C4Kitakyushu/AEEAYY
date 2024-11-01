e66a2c627cc1aecc8ef6361999ea33520096a4c6
Gestures5 <kenshimadax@gmail.com>
2024-11-02 00:52:32 +0800

    Update flux.js

--- commands/flux.js
+++ commands/flux.js
@@ -5,9 +5,9 @@ const { sendMessage } = require('../handles/sendMessage');
 module.exports = {
   // Metadata for the command
   name: 'flux',  // Command name
-  description: 'generate image based on your prompt.',  // Description
-  usage: '!generateImage <prompt>',  // Usage
-  author: 'developer',  // Author of the command
+  description: 'generates an image based on a prompt.',  // description 
+  usage: '/generateImage <prompt>',  // usage
+  author: 'YourName',  // Author of the command
 
   // Main function that executes the command
   async execute(senderId, args, pageAccessToken) {
@@ -15,17 +15,17 @@ module.exports = {
     if (!args || args.length === 0) {
       // Send message requesting a prompt if missing
       await sendMessage(senderId, {
-        text: '❌ 𝗣𝗹𝗲𝗮𝘀𝗲 𝗽𝗿𝗼𝘃𝗶𝗱𝗲 𝘆𝗼𝘂𝗿 𝗽𝗿𝗼𝗺𝗽𝘁\n\n𝗘𝘅𝗮𝗺𝗽𝗹𝗲: 𝗳𝗹𝘂𝘅 𝗱𝗼𝗴'
+        text: '❌ 𝗣𝗹𝗲𝗮𝘀𝗲 𝗽𝗿𝗼𝘃𝗶𝗱𝗲 𝘆𝗼𝘂𝗿 𝗽𝗿𝗼𝗺𝗽𝘁\n\n𝗘𝘅𝗮𝗺𝗽𝗹𝗲: 𝗳𝗹𝘂𝘅 𝗱𝗼𝗴'.'
       }, pageAccessToken);
       return;  // Exit the function if no prompt is provided
     }
 
     // Concatenate arguments to form the prompt
     const prompt = args.join(' ');
-    const apiUrl = `https://joshweb.click/api/flux?prompt=${encodeURIComponent(prompt)}&model=4`;  // API endpoint with the prompt
+    const apiUrl = `https://api.kenliejugarap.com/flux/?width=1024&height=1024&prompt=${encodeURIComponent(prompt)}`;  // API endpoint with the prompt
 
     // Notify user that the image is being generated
-    await sendMessage(senderId, { text: '⌛ 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗶𝗻𝗴 𝗶𝗺𝗮𝗴𝗲 𝗯𝗮𝘀𝗲𝗱 𝗼𝗻 𝘆𝗼𝘂𝗿 𝗽𝗿𝗼𝗺𝗽𝘁, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁....' }, pageAccessToken);
+    await sendMessage(senderId, { text: '⌛ 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗶𝗻𝗴 𝗶𝗺𝗮𝗴𝗲 𝗯𝗮𝘀𝗲𝗱 𝗼𝗻 𝘆𝗼𝘂𝗿 𝗽𝗿𝗼𝗺𝗽𝘁, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁.' }, pageAccessToken);
 
     try {
       // Send the generated image to the user as an attachment
@@ -44,7 +44,7 @@ module.exports = {
       
       // Notify user of the error
       await sendMessage(senderId, {
-        text: 'Sorry, an error occurred while generating the image.'
+        text: 'There was an error generating the image. Please try again later.'
       }, pageAccessToken);
     }
   }

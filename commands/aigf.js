6b03357e6b7cb73b689bb12c39863dbbdfc62689
Gestures5 <kenshimadax@gmail.com>
2024-11-09 13:03:18 +0800

    Update aigf.js

--- commands/aigf.js
+++ commands/aigf.js
@@ -8,15 +8,17 @@ module.exports = {
     const userInput = args.join(' ').trim();
 
     if (!userInput) {
-      return sendMessage(senderId, { text: '😁uhmm.. hello baby how are you today? I'm glad to meet you again😚🥰' }, pageAccessToken);
+      return sendMessage(senderId, { text: '❌ 𝗣𝗹𝗲𝗮𝘀𝗲 𝗽𝗿𝗼𝘃𝗶𝗱𝗲 𝘆𝗼𝘂𝗿 𝗶𝗻𝗽𝘂𝘁.\n\n𝗘𝘅𝗮𝗺𝗽𝗹𝗲: 𝗳𝗹𝗶𝗿𝘁 𝗺𝗲 𝗹𝗼𝘃𝗲😚' }, pageAccessToken);
     }
+
+    sendMessage(senderId, { text: '⌛ AI girlfriend is processing your input, please wait...' }, pageAccessToken);
 
     try {
       const response = await axios.get('https://joshweb.click/api/ai-gf', {
         params: { q: userInput }
       });
       const responseData = response.data;
-      const responseString = responseData.result ? responseData.result : 'No result found.';
+      const responseString = responseData.result ? responseData.result : '❌ No result found.';
 
       const formattedResponse = `
 💬 AI Girlfriend Conversation

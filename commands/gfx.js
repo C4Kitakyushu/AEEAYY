de30059342ffcbc32e0a375018d464b07a135cb5
Gestures5 <kenshimadax@gmail.com>
2024-11-09 16:13:30 +0800

    Update commands

---
+++ commands/gfx.js
@@ -0,0 +1,29 @@
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'gfx',
+  description: 'gfx <text>',
+  usage: 'gfx <text>',
+  author: 'developer',
+  async execute(senderId, args, pageAccessToken) {
+    if (!args || !Array.isArray(args) || args.length === 0) {
+      await sendMessage(senderId, { text: '❌ Please provide the text ' }, pageAccessToken);
+      return;
+    }
+
+    const text = args.join(' ');
+    const apiUrl = `https://api-canvass.vercel.app/gfx?text=${encodeURIComponent(text)}`;
+
+    try {
+      await sendMessage(senderId, { 
+        attachment: { 
+          type: 'image', 
+          payload: { url: apiUrl } 
+        } 
+      }, pageAccessToken);
+    } catch (error) {
+      console.error('Error:', error);
+      await sendMessage(senderId, { text: 'Error: Could not generate canvas image.' }, pageAccessToken);
+    }
+  }
+};

ad006776c055745d5e0e872166a9b5fc8e29002b
Gestures5 <kenshimadax@gmail.com>
2024-11-10 22:11:31 +0800

    Update morphic.js

--- commands/morphic.js
+++ commands/morphic.js
@@ -1,27 +1,27 @@
 const axios = require('axios');
 
 module.exports = {
-  name: 'morphic',
-  description: 'interact with morphic ai',
+  name: 'llama',
+  description: 'interact to llama 3 ai',
   author: 'developer',
   async execute(senderId, args, pageAccessToken, sendMessage) {
     const userInput = args.join(' ').trim();
 
     if (!userInput) {
-      return sendMessage(senderId, { text: '❌ 𝗣𝗹𝗲𝗮𝘀𝗲 𝗽𝗿𝗼𝘃𝗶𝗱𝗲 𝘆𝗼𝘂𝗿 𝗾𝘂𝗲𝘀𝘁𝗶𝗼𝗻𝘀.\n𝗘𝘅𝗮𝗺𝗽𝗹𝗲: 𝗪𝗵𝗮𝘁 𝗶𝘀 𝗠𝗼𝗿𝗽𝗵𝗶𝗰 𝗔𝗜?' }, pageAccessToken);
+      return sendMessage(senderId, { text: '❌ 𝗣𝗹𝗲𝗮𝘀𝗲 𝗽𝗿𝗼𝘃𝗶𝗱𝗲 𝘆𝗼𝘂𝗿 𝗾𝘂𝗲𝘀𝘁𝗶𝗼𝗻𝘀\n𝗘𝘅𝗮𝗺𝗽𝗹𝗲: 𝗪𝗵𝗼 𝗮𝗿𝗲 𝘆𝗼𝘂?' }, pageAccessToken);
     }
 
-    sendMessage(senderId, { text: '⌛ 𝗠𝗼𝗿𝗽𝗵𝗶𝗰 𝗔𝗜 𝗽𝗿𝗼𝗰𝗲𝘀𝘀𝗶𝗻𝗴 𝘆𝗼𝘂𝗿 𝗿𝗲𝗾𝘂𝗲𝘀𝘁, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁...' }, pageAccessToken);
+    sendMessage(senderId, { text: '⌛ 𝗟𝗹𝗮𝗺𝗮 𝟯 𝘀𝗲𝗮𝗿𝗰𝗵𝗶𝗻𝗴 𝘆𝗼𝘂𝗿 𝗾𝘂𝗲𝘀𝘁𝗶𝗼𝗻𝘀, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁...' }, pageAccessToken);
 
     try {
-      const response = await axios.get('https://joshweb.click/api/morphic', {
+      const response = await axios.get('https://joshweb.click/api/llama-3-70b', {
         params: { q: userInput }
       });
-      const morphicResponse = response.data;
-      const responseString = morphicResponse.result ? morphicResponse.result : '𝗡𝗼 𝗿𝗲𝘀𝘂𝗹𝘁 𝗳𝗼𝘂𝗻𝗱.';
+      const llamaResponse = response.data;
+      const responseString = llamaResponse.result ? llamaResponse.result : '𝗡𝗼 𝗿𝗲𝘀𝘂𝗹𝘁 𝗳𝗼𝘂𝗻𝗱.';
 
       const formattedResponse = `
-🤖 𝗠𝗼𝗿𝗽𝗵𝗶𝗰 𝗔𝗜 𝗖𝗼𝗻𝘃𝗲𝗿𝘀𝗮𝘁𝗶𝗼𝗻𝗮𝗹
+🦙 𝗟𝗹𝗮𝗺𝗮 𝟯 𝗖𝗼𝗻𝘃𝗲𝗿𝘀𝗮𝘁𝗶𝗼𝗻𝗮𝗹
 ━━━━━━━━━━━━━━━━━━
 ${responseString}
 ━━━━━━━━━━━━━━━━━━

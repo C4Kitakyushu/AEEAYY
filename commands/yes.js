f7f95f5b7d84f42bb5b37499b7522270af82056b
Gestures5 <kenshimadax@gmail.com>
2024-11-11 19:43:15 +0800

    Update commands

---
+++ commands/yes.js
@@ -0,0 +1,29 @@
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'yes',
+  description: 'yes <text>',
+  usage: 'yes <text>',
+  author: 'developer',
+  async execute(senderId, args, pageAccessToken) {
+    if (!args || !Array.isArray(args) || args.length === 0) {
+      await sendMessage(senderId, { text: '❌ Please provide the text ' }, pageAccessToken);
+      return;
+    }
+
+    const text = args.join(' ');
+    const apiUrl = `https://jerome-web.onrender.com/service/api/yes?text=${encodeURIComponent(text)}`;
+
+    try {
+      await sendMessage(senderId, { 
+        attachment: { 
+          type: 'image', 
+          payload: { url: apiUrl } 
+        } 
+      }, pageAccessToken);
+    } catch (error) {
+      console.error('Error:', error);
+      await sendMessage(senderId, { text: 'Error: Could not generate canvas image.' }, pageAccessToken);
+    }
+  }
+};

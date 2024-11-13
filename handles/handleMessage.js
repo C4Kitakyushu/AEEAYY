9026f939d71e288a080070470ed8cf01ad177818
Gestures5 <kenshimadax@gmail.com>
2024-11-13 18:27:34 +0800

    Update handleMessage.js

--- handles/handleMessage.js
+++ handles/handleMessage.js
@@ -102,20 +102,18 @@ if (messageText === 'imgur') {
       }
       return;
     }
-// Handling "enhanceimage" command
-if (messageText === 'enhanceimage') {
+// Handling "upscale" command
+if (messageText === 'upscale') {
   const lastImage = lastImageByUser.get(senderId);
   if (lastImage) {
     try {
-      await commands.get('enhanceImage').execute(senderId, [], pageAccessToken, lastImage);
+      await commands.get('upscale').execute(senderId, [], pageAccessToken, lastImage);
       lastImageByUser.delete(senderId);
     } catch (error) {
       await sendMessage(senderId, { text: 'An error occurred while processing the image.' }, pageAccessToken);
     }
   } else {
-    await sendMessage(senderId, { 
-      text: '❌ 𝗣𝗹𝗲𝗮𝘀𝗲 𝘀𝗲𝗻𝗱 𝗮𝗻 𝗶𝗺𝗮𝗴𝗲 𝗳𝗶𝗿𝘀𝘁, 𝘁𝗵𝗲𝗻 𝘁𝘆𝗽𝗲 "𝗲𝗻𝗵𝗮𝗻𝗰𝗲𝗶𝗺𝗮𝗴𝗲" 𝘁𝗼 𝗲𝗻𝗵𝗮𝗻𝗰𝗲 𝗶𝘁𝘀 𝗾𝘂𝗮𝗹𝗶𝘁𝘆.'
-    }, pageAccessToken);
+    await sendMessage(senderId, { text: '❌ 𝗣𝗹𝗲𝗮𝘀𝗲 𝘀𝗲𝗻𝗱 𝗮𝗻 𝗶𝗺𝗮𝗴𝗲 𝗳𝗶𝗿𝘀𝘁, 𝘁𝗵𝗲𝗻 𝘁𝘆𝗽𝗲 "𝘂𝗽𝘀𝗰𝗮𝗹𝗲" 𝘁𝗼 𝘂𝗽𝘀𝗰𝗮𝗹𝗲 𝗶𝘁.' }, pageAccessToken);
   }
   return;
 }

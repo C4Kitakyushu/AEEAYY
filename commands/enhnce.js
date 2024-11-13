bb27782f95dac473e03e34edf2aee83bbd5d725b
Gestures5 <kenshimadax@gmail.com>
2024-11-13 18:26:25 +0800

    Update enhnce.js

--- commands/enhnce.js
+++ commands/enhnce.js
@@ -2,33 +2,34 @@ const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: 'enhanceImage',
-  description: 'Enhance image quality using the Remini API.',
-  author: 'chi',
+  name: 'upscale',
+  description: 'Upscale an image.',
+  author: 'developer',
 
   async execute(senderId, args, pageAccessToken, imageUrl) {
     if (!imageUrl) {
       return sendMessage(senderId, {
-        text: `𝗣𝗹𝗲𝗮𝘀𝗲 𝘀𝗲𝗻𝗱 𝗮𝗻 𝗶𝗺𝗮𝗴𝗲 𝗳𝗶𝗿𝘀𝘁, 𝘁𝗵𝗲𝗻 𝘁𝘆𝗽𝗲 "𝗲𝗻𝗵𝗮𝗻𝗰𝗲𝗶𝗺𝗮𝗴𝗲" 𝘁𝗼 𝗲𝗻𝗵𝗮𝗻𝗰𝗲 𝗶𝘁𝘀 𝗾𝘂𝗮𝗹𝗶𝘁𝘆.`
+        text: `𝗣𝗹𝗲𝗮𝘀𝗲 𝘀𝗲𝗻𝗱 𝗮𝗻 𝗶𝗺𝗮𝗴𝗲 𝗳𝗶𝗿𝘀𝘁, 𝘁𝗵𝗲𝗻 𝘁𝘆𝗽𝗲 "𝘂𝗽𝘀𝗰𝗮𝗹𝗲" 𝘁𝗼 𝘂𝗽𝘀𝗰𝗮𝗹𝗲 𝗶𝘁.`
       }, pageAccessToken);
     }
 
-    await sendMessage(senderId, { text: '⌛ 𝗘𝗻𝗵𝗮𝗻𝗰𝗶𝗻𝗴 𝗶𝗺𝗮𝗴𝗲 𝗾𝘂𝗮𝗹𝗶𝘁𝘆, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁...' }, pageAccessToken);
+    await sendMessage(senderId, { text: '⌛ 𝗨𝗽𝘀𝗰𝗮𝗹𝗶𝗻𝗴 𝗶𝗺𝗮𝗴𝗲, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁...' }, pageAccessToken);
 
     try {
-      const enhanceImageUrl = `https://betadash-uploader.vercel.app/remini?url=${encodeURIComponent(imageUrl)}`;
+      const upscaleUrl = `https://appjonellccapis.zapto.org/api/upscale?url=${encodeURIComponent(imageUrl)}`;
 
+      // Send the upscaled image to the user
       await sendMessage(senderId, {
         attachment: {
           type: 'image',
           payload: {
-            url: enhanceImageUrl
+            url: upscaleUrl
           }
         }
       }, pageAccessToken);
 
     } catch (error) {
-      console.error('Error enhancing image:', error);
+      console.error('Error upscaling image:', error);
       await sendMessage(senderId, {
         text: 'An error occurred while processing the image. Please try again later.'
       }, pageAccessToken);

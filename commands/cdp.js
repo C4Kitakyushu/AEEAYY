e03645d5d620d478f74ec4061aae1ecef7e6dbcb
Gestures5 <kenshimadax@gmail.com>
2024-11-14 18:25:33 +0800

    Update cdp.js

--- commands/cdp.js
+++ commands/cdp.js
@@ -7,7 +7,7 @@ module.exports = {
   usage: 'get cdp pics',
   author: 'developer',
   async execute(senderId, args, pageAccessToken) {
-    const apiUrl = 'https://c-v1.onrender.com/v1/cdp/get';
+    const apiUrl = 'https://c-v1.onrender.com/api/cdp';
 
     try {
       // Send a message indicating that the image is being fetched

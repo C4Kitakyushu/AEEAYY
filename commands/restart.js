b14bf5f8665bac1be085ce3edd25b32d8c3b46e2
Gestures5 <kenshimadax@gmail.com>
2024-11-18 20:11:35 +0800

    Update commands

---
+++ commands/restart.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'restart',
+  description: 'restart bot status (admin access only)',
+  author: 'chrnyy',
+
+  async execute(senderId, args, pageAccessToken, event) {
+    const adminId = '100068434233610';
+
+    if (senderId !== adminId) {
+      return sendMessage(senderId, { text: '❌ Admin access only.' }, pageAccessToken);
+    }
+
+    try {
+      await sendMessage(senderId, { text: '⚙️ MCv2 is restarting please wait..' }, pageAccessToken);
+
+      const restartFile = './restart.json';
+      fs.writeFileSync(restartFile, JSON.stringify({ restartId: senderId, time: Date.now() }, null, 4));
+
+      setTimeout(() => {
+        console.log('Exiting process...');
+        process.exit(1);
+      }, 2000);
+    } catch (error) {
+      console.error('❌ Error executing the restart command:', error);
+      await sendMessage(senderId, { text: '❌ There was an error executing the command "restart". Please try again later.' }, pageAccessToken);
+    }
+  }
+};

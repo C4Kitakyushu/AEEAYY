f5e02a3fd2c0a81665766e9dd85a3c0795013f7f
Gestures5 <kenshimadax@gmail.com>
2024-11-23 09:10:12 +0800

    Update commands

---
+++ commands/freesms.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+
+module.exports = {
+  name: 'freesms',
+  description: 'freesms <phoneNumber> <message>.',
+  author: 'developer',
+  async execute(senderId, args, pageAccessToken, sendMessage) {
+    const phoneNumber = args[0];
+    const message = args.slice(1).join(' ');
+
+    if (!phoneNumber || !message) {
+      return sendMessage(senderId, { text: 'Usage: freesms [phone number] [message]' }, pageAccessToken);
+    }
+
+    sendMessage(senderId, { text: '⏳ 𝗣𝗿𝗼𝗰𝗲𝘀𝘀𝗶𝗻𝗴 𝘆𝗼𝘂𝗿 𝗿𝗲𝗾𝘂𝗲𝘀𝘁 𝘁𝗼 𝘀𝗲𝗻𝗱 𝘀𝗺𝘀, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁...' }, pageAccessToken);
+
+    try {
+      const response = await axios.get('https://api.kenliejugarap.com/freesmslbc/', {
+        params: {
+          number: phoneNumber,
+          message: encodeURIComponent(message)
+        }
+      });
+      console.log('Response:', response.data);
+      sendMessage(senderId, { text: '𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗵𝗮𝘀 𝗯𝗲𝗲𝗻 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝘀𝗲𝗻𝘁 ✅' }, pageAccessToken);
+    } catch (error) {
+      console.error('Error:', error);
+      sendMessage(senderId, { text: '❌ Failed to send the message.' }, pageAccessToken);
+    }
+  }
+};

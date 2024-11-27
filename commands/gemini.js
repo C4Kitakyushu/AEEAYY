780aa7021c99bde5192ab8798c60db96629cf701
Gestures5 <kenshimadax@gmail.com>
2024-11-27 20:26:00 +0800

    Update gemini.js

--- commands/gemini.js
+++ commands/gemini.js
@@ -27,7 +27,7 @@ module.exports = {
       }
 
       const apiUrl = `https://jerome-web.gleeze.com/service/api/gemini?`;
-      const response = await handleImageRecognition(apiUrl, userPrompt, imageUrl);
+      const response = await handleImageRecognition(apiUrl, ask, imageUrl);
       const result = response.gemini;
 
       // Get the current response time in Manila timezone

cd3444851dcb5f6ab7aa75d178a8f515718fcdc7
Gestures5 <kenshimadax@gmail.com>
2024-12-06 20:08:42 +0800

    Update getlink.js

--- commands/getlink.js
+++ commands/getlink.js
@@ -1,31 +1,3 @@
-if (messageText === 'getlink') {
-  const lastImage = lastImageByUser.get(senderId);
-  const lastVideo = lastVideoByUser.get(senderId);
-  const mediaToRetrieve = lastImage || lastVideo;
-
-  if (mediaToRetrieve) {
-    try {
-      // Execute the 'getlink' command to retrieve the media link
-      await commands.get('getlink').execute(senderId, [], pageAccessToken, {
-        messageReply: { attachments: [{ url: mediaToRetrieve }] }
-      });
-
-      // Clear the last media references for the user
-      lastImageByUser.delete(senderId);
-      lastVideoByUser.delete(senderId);
-    } catch (error) {
-      console.error('Error while retrieving the media link:', error.message);
-      await sendMessage(senderId, { text: '🫵😼 Something went wrong. Please try again.' }, pageAccessToken);
-    }
-  } else {
-    // Inform the user to send media before using the 'getlink' command
-    await sendMessage(senderId, {
-      text: '❌ 𝗣𝗹𝗲𝗮𝘀𝗲 𝘀𝗲𝗻𝗱 𝗮𝗻 𝗶𝗺𝗮𝗴𝗲 𝗼𝗿 𝘃𝗶𝗱𝗲𝗼 𝗳𝗶𝗿𝘀𝘁, 𝘁𝗵𝗲𝗻 𝘁𝘆𝗽𝗲 "𝗴𝗲𝘁𝗹𝗶𝗻𝗸" 𝘁𝗼 𝗿𝗲𝘁𝗿𝗶𝗲𝘃𝗲 𝘁𝗵𝗲 𝗹𝗶𝗻𝗸.'
-    }, pageAccessToken);
-  }
-  return;
-}
-
 module.exports = {
   name: 'getlink',
   description: 'Retrieve the URL of an image or video attachment.',

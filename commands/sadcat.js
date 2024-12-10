e485fb26961798320a7aa79e1f8e231165a07712
Gestures5 <kenshimadax@gmail.com>
2024-12-10 08:38:03 +0800

    Update commands

---
+++ commands/sadcat.js
@@ -0,0 +1,29 @@
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'sadcat',
+  description: 'sadcat <text>',
+  usage: 'malongcote <text>',
+  author: 'developer',
+  async execute(senderId, args, pageAccessToken) {
+    if (!args || !Array.isArray(args) || args.length === 0) {
+      await sendMessage(senderId, { text: '❌ Please provide the text ' }, pageAccessToken);
+      return;
+    }
+
+    const text = args.join(' ');
+    const apiUrl = `https://kaiz-apis.gleeze.com/api/sadcat?text=${encodeURIComponent(text)}`;
+
+    try {
+      await sendMessage(senderId, { 
+        attachment: { 
+          type: 'image', 
+          payload: { url: apiUrl } 
+        } 
+      }, pageAccessToken);
+    } catch (error) {
+      console.error('Error:', error);
+      await sendMessage(senderId, { text: 'Error: Could not generate billboard image.' }, pageAccessToken);
+    }
+  }
+};

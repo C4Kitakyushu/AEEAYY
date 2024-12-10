c18efb913787dcdf4cb920182ac981c76000b15b
Gestures5 <kenshimadax@gmail.com>
2024-12-10 19:19:32 +0800

    Update upscale.js

--- commands/upscale.js
+++ commands/upscale.js
@@ -1,41 +1,36 @@
-const axios = require("axios");
-const { sendMessage } = require("../handles/sendMessage");
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: "upscale",
-  description: "enhance image quality",
-  author: "developer",
+  name: 'upscale',
+  description: 'upscale an image.',
+  author: 'Harith',
 
   async execute(senderId, args, pageAccessToken, imageUrl) {
-    // Check if an image URL is provided
     if (!imageUrl) {
       return sendMessage(senderId, {
-        text: `❌ 𝗣𝗹𝗲𝗮𝘀𝗲 𝘀𝗲𝗻𝗱 𝗮𝗻 𝗶𝗺𝗮𝗴𝗲 𝗳𝗶𝗿𝘀𝘁, 𝘁𝗵𝗲𝗻 𝘁𝘆𝗽𝗲 "𝗿𝗲𝗺𝗶𝗻𝗶" 𝘁𝗼 𝗲𝗻𝗵𝗮𝗻𝗰𝗲 𝗶𝘁.`
+        text: `𝗣𝗹𝗲𝗮𝘀𝗲 𝘀𝗲𝗻𝗱 𝗮𝗻 𝗶𝗺𝗮𝗴𝗲 𝗳𝗶𝗿𝘀𝘁, 𝘁𝗵𝗲𝗻 𝘁𝘆𝗽𝗲 "𝘂𝗽𝘀𝗰𝗮𝗹𝗲" 𝘁𝗼 𝗶𝗻𝗰𝗿𝗲𝗮𝘀𝗲 𝗶𝘁𝘀 𝗿𝗲𝘀𝗼𝗹𝘂𝘁𝗶𝗼𝗻.`
       }, pageAccessToken);
     }
 
-    // Notify the user that enhancement is in progress
-    sendMessage(senderId, { text: "⌛ 𝗘𝗻𝗵𝗮𝗻𝗰𝗶𝗻𝗴 𝗶𝗺𝗮𝗴𝗲 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁...." }, pageAccessToken);
+    await sendMessage(senderId, { text: '⌛ 𝗨𝗽𝘀𝗰𝗮𝗹𝗶𝗻𝗴 𝗶𝗺𝗮𝗴𝗲, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁...' }, pageAccessToken);
 
     try {
-      // Fetch the enhanced image from the API
-      const response = await axios.get(`https://hiroshi-api.onrender.com/image/upscale?url=${encodeURIComponent(imageUrl)}`);
-      const processedImageURL = response.data;
+      const upscaleUrl = `https://ccprojectapis.ddns.net/api/upscale?url=${encodeURIComponent(imageUrl)}`;
 
-      // Send the enhanced image URL back to the user
       await sendMessage(senderId, {
         attachment: {
-          type: "image",
+          type: 'image',
           payload: {
-            url: processedImageURL
+            url: upscaleUrl
           }
         }
       }, pageAccessToken);
 
     } catch (error) {
-      console.error("❌ Error processing image:", error);
+      console.error('Error upscaling image:', error);
       await sendMessage(senderId, {
-        text: `❌ An error occurred while processing the image. Please try again later.`
+        text: 'An error occurred while processing the image. Please try again later.'
       }, pageAccessToken);
     }
   }

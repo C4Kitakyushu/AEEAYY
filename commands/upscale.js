d437aa9f1238ddb832d5962efe464252ef639c38
Gestures5 <kenshimadax@gmail.com>
2024-12-18 07:52:58 +0800

    Update upscale.js

--- commands/upscale.js
+++ commands/upscale.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: 'remini',
+  name: 'upscale',
   description: 'Enhance image quality using AI.',
   author: 'Rized',
 

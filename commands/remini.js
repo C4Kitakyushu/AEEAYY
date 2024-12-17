8339c5b255917353d12946070a1a067dd5ea9259
Gestures5 <kenshimadax@gmail.com>
2024-12-18 07:27:35 +0800

    Update remini.js

--- commands/remini.js
+++ commands/remini.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const { sendMessage } = require("../handles/sendMessage");
 
 module.exports = {
-  name: "upscales",
+  name: "remini",
   description: "Enhanced photo",
   author: "Jonell Magallanes",
   usage: "Reply to an image with 'remini' to enhance it",

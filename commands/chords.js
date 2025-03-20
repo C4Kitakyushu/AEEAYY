9ce75e19076fc1b5c7aa2a7b4a9822be571b4c45
Gestures5 <kenshimadax@gmail.com>
2025-03-20 13:13:13 +0800

    Update chords.js

--- commands/chords.js
+++ commands/chords.js
@@ -16,7 +16,7 @@ module.exports = {
         const chordsMessage = `Title: ${result.title}\nArtist: ${result.artist}\nKey: ${result.key}\n\n${result.chords}`;
 
         // Split the response into chunks if it exceeds 2000 characters
-        const maxMessageLength = 2000;
+        const maxMessageLength = 600;
         if (chordsMessage.length > maxMessageLength) {
           const messages = splitMessageIntoChunks(chordsMessage, maxMessageLength);
           for (const message of messages) {
@@ -41,7 +41,7 @@ module.exports = {
 };
 
 function splitMessageIntoChunks(message, chunkSize) {
-  const chunks = 600;
+  const chunks = [];
   for (let i = 0; i < message.length; i += chunkSize) {
     chunks.push(message.slice(i, i + chunkSize));
   }

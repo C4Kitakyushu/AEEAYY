64d0b1bc003f8b2ef76bdbc24f942f26614ea042
Gestures5 <kenshimadax@gmail.com>
2025-03-23 22:48:38 +0800

    Update handleMessage.js

--- handles/handleMessage.js
+++ handles/handleMessage.js
@@ -52,6 +52,23 @@ async function handleMessage(event, pageAccessToken) {
       }
       return;
     }
+
+//
+Handling "removebgv2" command
+    if (messageText === 'removebgv2') {
+      const lastImage = lastImageByUser.get(senderId);
+      if (lastImage) {
+        try {
+          await commands.get('removebgv2').execute(senderId, [], pageAccessToken, lastImage);
+          lastImageByUser.delete(senderId);
+        } catch (error) {
+          await sendMessage(senderId, { text: 'An error occurred while processing the image.' }, pageAccessToken);
+        }
+      } else {
+        await sendMessage(senderId, { text: '❌ 𝗣𝗹𝗲𝗮𝘀𝗲 𝘀𝗲𝗻𝗱 𝗮𝗻 𝗶𝗺𝗮𝗴𝗲 𝗳𝗶𝗿𝘀𝘁, 𝘁𝗵𝗲𝗻 𝘁𝘆𝗽𝗲 "𝗿𝗲𝗺𝗼𝘃𝗲𝗯𝗴" 𝘁𝗼 𝗿𝗲𝗺𝗼𝘃𝗲 𝗶𝘁𝘀 𝗯𝗮𝗰𝗸𝗴𝗿𝗼𝘂𝗻𝗱.' }, pageAccessToken);
+      }
+      return;
+    }
     // Handling "remini" command
     if (messageText === 'remini') {
       const lastImage = lastImageByUser.get(senderId);

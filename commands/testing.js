5f31b82d6d300daf2145ad181f3862440c734728
Gestures5 <kenshimadax@gmail.com>
2025-03-23 21:15:36 +0800

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -6,8 +6,8 @@ const token = fs.readFileSync('token.txt', 'utf8');
 
 module.exports = {
   name: 'test',
-  description: 'Fetch a random image using the Random API.',
-  usage: 'random',
+  description: 'Fetch a random TikTok random-jjk video.',
+  usage: 'Shoti',
   author: 'developer',
 
   execute: async (senderId) => {
@@ -17,22 +17,25 @@ module.exports = {
     try {
       const { data } = await axios.get(apiUrl);
 
-      // Assuming the API returns a JSON with "status" and "randomUrl"
-      if (data.status === 'success' && data.randomUrl) {
-        const imageMessage = {
+      if (data.status === 'success' && data.random-jjk) {
+        const { videoUrl } = data.random-jjk;
+
+        const videoMessage = {
           attachment: {
-            type: 'image',
-            payload: { url: data.randomUrl }
-          }
+            type: 'video',
+            payload: {
+              url: videoUrl,
+            },
+          },
         };
 
-        sendMessage(senderId, imageMessage, pageAccessToken);
+        await sendMessage(senderId, videoMessage, pageAccessToken);
       } else {
-        sendError(senderId, 'Error: Unable to fetch random content.', pageAccessToken);
+        sendError(senderId, '❌ Error: Unable to fetch Random-jjk video.', pageAccessToken);
       }
     } catch (error) {
-      console.error('Error fetching random content:', error);
-      sendError(senderId, 'Error: Unexpected error occurred.', pageAccessToken);
+      console.error('Error fetching Random-jjk video:', error);
+      sendError(senderId, '❌ Error: Unexpected error occurred.', pageAccessToken);
     }
   },
 };
